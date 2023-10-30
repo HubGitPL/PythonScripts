ca116e9b9d45735161c877f9404213145d43f691
Mateusz <72828637+HubGitPL@users.noreply.github.com>
2023-10-30 13:36:18 +0100

    Create quizGenerator.py
    v 1.0

---
+++ quizGenerator.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import os
+import random
+
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+'Arkansas': 'Little Rock', 'Kalifornia': 'Sacramento', 'Kolorado': 'Denver',
+'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Floryda': 'Tallahassee',
+'Georgia': 'Atlanta', 'Hawaje': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+'Topeka', 'Kentucky': 'Frankfort', 'Luizjana': 'Baton Rouge', 'Maine':
+'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+'Nowy Meksyk': 'Santa Fe', 'Nowy Jork': 'Albany', 'Karolina Północna':
+'Raleigh', 'Dakota Północna': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma':
+'Oklahoma City', 'Oregon': 'Salem', 'Pensylwania': 'Harrisburg',
+'Rhode Island': 'Providence', 'Karolina Południowa': 'Columbia',
+'Dakota Południowa': 'Pierre', 'Tennessee': 'Nashville', 'Teksas': 'Austin',
+'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Wirginia': 'Richmond',
+'Waszyngton': 'Olympia', 'Wirginia Zachodnia': 'Charleston', 'Wisconsin':
+'Madison', 'Wyoming': 'Cheyenne'}
+numberOfQuizes = 35
+for quizNum in range(numberOfQuizes):
+    
+
+
