ec1d8cd809face5d3ab96883370e53f7fa5d864b
Mateusz <72828637+HubGitPL@users.noreply.github.com>
2023-11-01 20:35:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from pathlib import Path
 
 p=Path.home()
 shutil.copy(p / 'spam.txt', p / 'Downloads')
+#kopiuje zawartosc spam.txt do podanego folderu i zmienia jego nazwe
+shutil.copy(p / 'spam.txt', p / 'Downloads/spam2.txt')
