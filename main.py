0b07eb9e9e4f1f4498acc0f052701265943364ea
Mateusz <72828637+HubGitPL@users.noreply.github.com>
2023-11-02 17:57:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import shutil, os
 from pathlib import Path
 
-p=Path.home()
-shutil.copy(p / 'spam.txt', p / 'Downloads')
+p=Path.home() / 'studia'
+#shutil.copy(p / 'spam.txt', p / 'Downloads')
 #kopiuje zawartosc spam.txt do podanego folderu i zmienia jego nazwe
-shutil.copy(p / 'spam.txt', p / 'Downloads/spam2.txt')
+#shutil.copy(p / 'spam.txt', p / 'Downloads/spam2.txt')
+#kopiuje cały katalog w inne miejsce
+#hutil.copytree(p / 'Public', p / 'Downloads/public')
+
+for folderName, subfolders, filenames in os.walk(p):
+    print("Katalog bieżący to: " + folderName)
+
+    for subfolder in subfolders:
+        print(f'PODKATALOG KATALOGU {folderName}: ' + subfolder)
+
+    for filename in filenames:
+        print(f'PLIK KATALOGU {folderName}: ' + filename)
+    print('')
