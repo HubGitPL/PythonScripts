4e016d4d45991979928f6363d4baeb5469c9c40d
Mateusz <72828637+HubGitPL@users.noreply.github.com>
2023-11-03 14:08:08 +0100

    Update renameDates.py
    ended - everything works well

--- renameDates.py
+++ renameDates.py
@@ -10,11 +10,21 @@ datePattern = re.compile(r"""^(.*?)
 
 for amerFilename in os.listdir('.'):
     mo = datePattern.search(amerFilename)
-    
+
     if mo == None:
         continue
+
     beforePart = mo.group(1)
     monthPart = mo.group(2)
     dayPart = mo.group(4)
     yearPart = mo.group(6)
     afterPart = mo.group(8)
+
+    euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
+
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+    print(f'Zmiana nazwy pliku "{amerFilename}" na "{euroFilename}"')
+    #shutil.move(amerFilename, euroFilename)
