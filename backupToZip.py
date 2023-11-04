c7eb0dd5e04508194678105c6ffb6da9e0c5c1d5
Mateusz <72828637+HubGitPL@users.noreply.github.com>
2023-11-04 14:25:23 +0100

    Create backupToZip.py

---
+++ backupToZip.py
@@ -0,0 +1,12 @@
+import zipfile, os
+
+def backupToZip(folder):
+    folder = os.path.abspath(folder)
+    
+    number = 1
+    while True:
+        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number = number + 1
+    
