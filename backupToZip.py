6517c13362cbad90c5c8b4a0c4691d480c652bf2
Mateusz <72828637+HubGitPL@users.noreply.github.com>
2023-11-04 21:42:17 +0100

    ended backupToZip.py
    works good, tabs weren't good....

--- backupToZip.py
+++ backupToZip.py
@@ -9,18 +9,19 @@ def backupToZip(folder):
         if not os.path.exists(zipFilename):
             break
         number = number + 1
-        print(f'Tworzenie archiwum {zipFilename}...')
-        backupZip = zipfile.ZipFile(zipFilename, 'w')
+    print(f'Tworzenie archiwum {zipFilename}...')
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
         #spacer po katalogach
-        for foldername, subfolders, filenames in os.walk(folder):
-            print(f'Dodawanie plików w {foldername}...')
-            backupZip.write(foldername)
+    for foldername, subfolders, filenames in os.walk(folder):
+        print(f'Dodawanie plików w {foldername}...')
+        backupZip.write(foldername)
 
-            for filename in filenames:
-                newBase = os.path.basename(folder) + '_'
-                if filename.startswith(newBase) and filename.endswith('.zip'):
-                    continue
-                backupZip.write(os.path.join(foldername, filename))
-                backupZip.close()
-                print('Gotowe')
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
+    print('Gotowe')
+
 backupToZip(Path.home() / 'PycharmProjects')
