1c2e10bedbb956647cca4b7e8b9173f8ed8abf0a
Mateusz <72828637+HubGitPL@users.noreply.github.com>
2023-11-04 17:27:17 +0100

    Update backupToZip.py

--- backupToZip.py
+++ backupToZip.py
@@ -1,5 +1,5 @@
 import zipfile, os
-
+from pathlib import Path
 def backupToZip(folder):
     folder = os.path.abspath(folder)
 
@@ -11,3 +11,16 @@ def backupToZip(folder):
         number = number + 1
         print(f'Tworzenie archiwum {zipFilename}...')
         backupZip = zipfile.ZipFile(zipFilename, 'w')
+        #spacer po katalogach
+        for foldername, subfolders, filenames in os.walk(folder):
+            print(f'Dodawanie plików w {foldername}...')
+            backupZip.write(foldername)
+
+            for filename in filenames:
+                newBase = os.path.basename(folder) + '_'
+                if filename.startswith(newBase) and filename.endswith('.zip'):
+                    continue
+                backupZip.write(os.path.join(foldername, filename))
+                backupZip.close()
+                print('Gotowe')
+backupToZip(Path.home() / 'PycharmProjects')
